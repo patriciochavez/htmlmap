aa9c5a0310856ed4eccdcb077fdf2d2ae79f0fb8
patriciochavez <patricio_chavez@hotmail.com>
2017-03-25 02:43:58 -0300

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+var http = require('http');
+
+var location = new Object();
+
+var httpServer = http.createServer(app).listen(8080);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+        extended: true
+        }));
+
+app.get(/^(.+)$/, function(req, res){ 
+    switch(req.params[0]) {
+        case '/test':
+            res.send("Ok!");
+            break;
+        case '/':
+            res.sendFile( __dirname + 'pos.html');
+            break;            
+    default: //res.sendFile( __dirname + req.params[0]); 
+    }
+ });
+ 
